464778f7ebe9ae389126e0df166c3342b37a475f
Xintian Meng <mxtdsg@hotmail.com>
2018-01-19 23:55:34 -0800

    db added for single game, adding db for users(track wins/loses)

--- app.py
+++ app.py
@@ -1,45 +1,91 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-word = "BERKELEY"
-guesses = []
+db = SQLAlchemy(app)
+
+"""
+"""
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), default="xxx")
+    wins = db.Column(db.Integer, default=0)
+    loses = db.Column(db.Integer, default=0)
+    def __init__(self, username):
+        self.username = username
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer)
+    username = db.Column(db.String(10))
+    word = db.Column(db.String(50), default="BERKELEY")
+    guesses = db.Column(db.String(50), default='')
+    win = db.Column(db.Boolean, default=False)
+
+    # def __init__(self, user_id):
+        # self.user_id = user_id
+    def __init__(self, username):
+        self.username = username
+
+    def try_letter(self, letter):
+        self.guesses += letter
+        db.session.commit()
+
+    @property
+    def render(self):
+        rendered = ''.join([char if char in self.guesses else ' _ ' for c in self.word])
+        if rendered == word:
+            win = True
+            user = User.query.get(user_id)
+            user.wins+=1
+            db.session.commit()
+        return rendered
+
+    # @property
+    # def game_finished(self):
+
+
+
+def random_word():
+    return "BERKELEY"
+
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
-# @app.route('/play')
-# def play():
-#     return render_template('play.html', word = check(word, guesses))
 
-# def random10():
-#     return random.randrange(10)
-def randomword():
-    return "BERKELEY"
+@app.route('/play')
+def new_game():
+    # game = Game()
+    # game = Game(request.args.get('user'))
+    user = request.args.get('user')
+    user = User(user)
+    session['user_id'] = user.id
+    
+    game = Game(user.username)
+    db.session.add(game)
+    db.session.commit()
+    return redirect(url_for('play', game_id=game.id))
 
-def check(word, guesses):
-    re = ''
-    for char in word:
-        if char in guesses:
-            re += char
-        else:
-            re += "*"
-    return re
 
-@app.route('/play', methods=['GET', 'POST'])
-def play():
+@app.route('/play/<game_id>', methods=['GET', 'POST'])
+def play(game_id):
+    game = Game.query.get(game_id)
     if request.method == 'POST':
         letter = request.form['letter'].upper()
-        print letter
-        guesses.append(letter)
-    return render_template('play.html', word = check(word, guesses))
+        game.try_letter(letter)
+    return render_template('play.html', game=game)
+
+
 
 
 
