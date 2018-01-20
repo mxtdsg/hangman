13f9ada9b0655909417e22cdbe982d9eeb27e0c9
Xintian Meng <mxtdsg@hotmail.com>
2018-01-20 01:28:59 -0800

    fixed to one db to store users info and most recent game(no need to store all games)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import random
 
@@ -8,46 +8,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 
-"""
-"""
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(10), default="xxx")
-    wins = db.Column(db.Integer, default=0)
-    loses = db.Column(db.Integer, default=0)
-    def __init__(self, username):
-        self.username = username
 
 
 class Game(db.Model):
+    # user info
     id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer)
     username = db.Column(db.String(10))
-    word = db.Column(db.String(50), default="BERKELEY")
-    guesses = db.Column(db.String(50), default='')
-    win = db.Column(db.Boolean, default=False)
+    wins = db.Column(db.Integer, default=0)
+    loses = db.Column(db.Integer, default=0)
+    # most recent game
+    cur_word = db.Column(db.String(50), default="BERKELEY")
+    cur_guesses = db.Column(db.String(50), default='')
+    finished = db.Column(db.Boolean, default=False)
 
-    # def __init__(self, user_id):
-        # self.user_id = user_id
+
     def __init__(self, username):
         self.username = username
 
+    def new_game(self):
+        self.finished = False
+        self.cur_word = "STANFORD"
+        self.cur_guesses = ''
+
     def try_letter(self, letter):
-        self.guesses += letter
+        self.cur_guesses += letter
         db.session.commit()
 
     @property
     def render(self):
-        rendered = ''.join([char if char in self.guesses else ' _ ' for c in self.word])
-        if rendered == word:
-            win = True
-            user = User.query.get(user_id)
-            user.wins+=1
+        rendered = ''.join([char if char in self.cur_guesses else ' _ ' for char in self.cur_word])
+        if rendered == self.cur_word:
+
+            self.finished = True
+            self.wins+=1
             db.session.commit()
         return rendered
 
-    # @property
-    # def game_finished(self):
 
 
 
@@ -59,19 +55,20 @@ def random_word():
 
 @app.route('/')
 def index():
-    users = User.query.all()
+    users = Game.query.all()
     return render_template('index.html', users=users)
 
 
 @app.route('/play')
 def new_game():
-    # game = Game()
-    # game = Game(request.args.get('user'))
-    user = request.args.get('user')
-    user = User(user)
-    session['user_id'] = user.id
-    
-    game = Game(user.username)
+    username = request.args.get('username')
+
+    user = Game.query.filter_by(username=username).first()
+    if user is not None:
+        return redirect(url_for('play', game_id=user.id))
+
+
+    game = Game(username)
     db.session.add(game)
     db.session.commit()
     return redirect(url_for('play', game_id=game.id))
@@ -80,11 +77,12 @@ def new_game():
 @app.route('/play/<game_id>', methods=['GET', 'POST'])
 def play(game_id):
     game = Game.query.get(game_id)
+    if game.finished:
+        game.new_game()
     if request.method == 'POST':
         letter = request.form['letter'].upper()
         game.try_letter(letter)
     return render_template('play.html', game=game)
-
 
 
 
